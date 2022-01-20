cc2291eb6a6818b91f6f3f6939c4bc9c7a2fe7a9
maoneart <maone.miui@gmail.com>
2022-01-20 14:35:59 +0700

    first commit

--- dhani.js
+++ dhani.js
@@ -1003,7 +1003,7 @@ but = [
           { buttonId: `${prefix}bayar`, buttonText: { displayText: 'BAYAR' }, type: 1 },
           { buttonId: `${prefix}owner`, buttonText: { displayText: 'OWNER'}, type: 1 }
         ]
-        sendButImage(from, menu, teks, gambar, but)
+        sendButImage(from, menu, teks, tamnel, but)
 break
 
 case 'done':
